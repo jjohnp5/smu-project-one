a2f06484c026893df4a3bc987368c1242ce1855a
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-03-23 17:46:43 -0500

    updated weather icons to show by weather code and time of day

--- public/assets/js/weather.js
+++ public/assets/js/weather.js
@@ -1,10 +1,14 @@
-// Variable to hold the date range
+// Variables represent date and time data
 var currentDate = moment().format("YYYYMMDD00");
 var currentTime = moment().format("HH:mm");
 var nextWeek = moment().add(7, "days").format("YYYYMMDD00");
 var twoWeeks = moment().add(14, "days").format("YYYYMMDD00");
 var timeOfDay = "";
+
+// Variable to hold weather code returned from API
 var weatherCode;
+
+// Creates a new image of 90px by 90px
 var icon = new Image(90, 90);
 
 var weatherAPI = "44df1c912088b9675614938b52bcbd0e";
@@ -53,7 +57,7 @@ function getEventWeather(long, lat) {
                 break;
         }
         // Set the icons based on weather code
-        setWeatherIcon(weatherCode);
+        setWeatherIcon(weatherCode, tempConverted);
 
         console.log("sunrise: " + sunriseTime);
         console.log("sunset: " + sunsetTime);
@@ -63,50 +67,55 @@ function getEventWeather(long, lat) {
     });
 }
 
-function setWeatherIcon(code) {
-    console.log("inside setWeatherIcon");
+function setWeatherIcon(code, temp) {
+    // var timeHTML = "<span>Current Time: " + moment().format("H:mm") + "</span><br/>";
+    // var dateHTML = "<span>Current Date: " + moment().format("MM-DD-YYYY") + "</span><br/>";
+    var imgSrc = "./assets/images/weather/";
+    var tempHTML = "<span>Current Temp: " + temp + "&deg; </span>";
+
     if (timeOfDay === "day") {
-        console.log("Inside day switch");
         switch (code) {
             // Check for daytime thunderstorms
             case (code > 199):
             case (code < 300):
-                icon.src = "../images/weather/day_tstorms.ico";
+                icon.src = imgSrc + "day_tstorms.ico";
                 console.log("day thunderstorms");
                 break;
             // Check for daytime light rain
             case (code > 299):
             case (code < 400):
-                icon.src = "../images/weather/day_rain_light.ico";
+                icon.src = imgSrc + "day_rain_light.ico";
                 console.log("day light rain");
                 break;
             // Check for daytime heavy rain
             case (code > 499):
             case (code < 600):
-                icon.src = "../images/weather/day_rain_heavy.ico";
+                icon.src = imgSrc + "day_rain_heavy.ico";
                 console.log("day heavy rain");
                 break;
             // Check for environmental conditions i.e. tornado, dust storms, etc.
             case (code > 699):
             case (code < 800):
-                icon.src = "../images/weather/cloudy_heavy.ico";
+                icon.src = imgSrc + "cloudy_heavy.ico";
                 console.log("day clouds");
                 break;
             // Check for daytime clear skies
             case (code = 800):
-                icon.src = "../images/weather/day_clear.ico";
+                icon.src = imgSrc + "day_clear.ico";
                 console.log("day clear");
                 break;
             // Check for daytime light clouds
             case (code = 801):
             case (code = 803):
             case (code = 804):
-                icon.src = "../images/weather/day_cloudy_light.ico";
+                icon.src = "./assets/images/weather/cloudy_heavy.ico";
+                document.getElementById("weather").innerHTML = tempHTML;
+                document.getElementById("weather").appendChild(icon);
                 console.log("day light clouds");
                 break;
             // Check for daytime heavy clouds
             case (code = 802):
-                icon.src = "../images/weather/day_cloudy_heavy.ico";
+                icon.src = imgSrc + "day_cloudy_heavy.ico";
                 console.log("day heavy clouds");
                 break;
             default:
@@ -118,42 +127,42 @@ function setWeatherIcon(code) {
                 // Check for nighttime thunderstorms
                 case (code > 199):
                 case (code < 300):
-                    icon.src = "../images/weather/night_tstorms.ico";
+                    icon.src = imgSrc + "night_tstorms.ico";
                     console.log("night thunderstorms");
                     break;
-                // Check for daytime light rain
+                // Check for nighttime light rain
                 case (code > 299):
                 case (code < 400):
-                    icon.src = "../images/weather/night_rain_light.ico";
+                    icon.src = imgSrc + "night_rain_light.ico";
                     console.log("night light rain");
                     break;
-                // Check for daytime heavy rain
+                // Check for nighttime heavy rain
                 case (code > 499):
                 case (code < 600):
-                    icon.src = "../images/weather/night_rain_heavy.ico";
+                    icon.src = imgSrc + "night_rain_heavy.ico";
                     console.log("night heavy rain");
                     break;
                 // Check for environmental conditions i.e. tornado, dust storms, etc.
                 case (code > 699):
                 case (code < 800):
-                    icon.src = "../images/weather/cloudy_heavy.ico";
+                    icon.src = imgSrc + "cloudy_heavy.ico";
                     console.log("night clouds");
                     break;
-                // Check for daytime clear skies
+                // Check for nighttime clear skies
                 case (code = 800):
-                    icon.src = "../images/weather/night_clear.ico";
+                    icon.src = imgSrc + "night_clear.ico";
                     console.log("night clear");
                     break;
-                // Check for daytime light clouds
+                // Check for nighttime light clouds
                 case (code = 801):
                 case (code = 803):
                 case (code = 804):
-                    icon.src = "../images/weather/night_cloudy_light.ico";
+                    icon.src = imgSrc + "night_cloudy_light.ico";
                     console.log("night light clouds");
                     break;
-                // Check for daytime heavy clouds
+                // Check for nighttime heavy clouds
                 case (code = 802):
-                    icon.src = "../images/weather/night_cloudy_heavy.ico";
+                    icon.src = imgSrc + "night_cloudy_heavy.ico";
                     console.log("night heavy clouds");
                     break;
                 default:
