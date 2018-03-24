29e37fcb8342b198d20cd968a641636d1d70308f
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-03-24 11:27:32 -0500

    minor updates

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,9 +1,5 @@
 ; (function () {
-<<<<<<< HEAD
-
-=======
     $('.selected-event').hide();
->>>>>>> 1d3eca5db99a9fba63f91372c7676e010420bc48
     let user = "";
     let db = firebase.database();
     let provider = new firebase.auth.GoogleAuthProvider();
@@ -42,13 +38,13 @@
         $('.authentication').empty();
         $(document).off('click', '.add-to-event');
         $('.selected-event').empty().slideUp(500);
-    
+
         if (user) {
             $('.authentication').hide();
             // $('.logged-in-popup').hide();
             // $('.logged-in-only').off();
             $('.logged-in-only').show()
-            
+
             // .on('click', function(){
             //     $('.logged-in-popup').css({
             //         position: 'absolute', 
@@ -57,10 +53,10 @@
             //         right: '50px'
             // })('')
             console.log(user);
-            
+
             $('.logged-in-btn')
                 .popover({
-                content: `
+                    content: `
                     <div class="popover-container">
                         <div class="row">
                         <div class="col-12">
@@ -78,15 +74,15 @@
                         </div>
                     </div>
                 `,
-                
-                // $(`<div>`).append($(`<p>`).text(user.email)).append(
-                //     $('<button>').addClass('btn btn-outline-danger sign-out').text('Sign Out'))
-                // ,
-                placement: 'bottom',
-                title: user.displayName,
-                html: true
-            })
-            
+
+                    // $(`<div>`).append($(`<p>`).text(user.email)).append(
+                    //     $('<button>').addClass('btn btn-outline-danger sign-out').text('Sign Out'))
+                    // ,
+                    placement: 'bottom',
+                    title: user.displayName,
+                    html: true
+                })
+
             // })
             // $('.authentication').append($('<button class="sign-out btn btn-outline-danger my-2 my-sm-0">').text("Sign Out"));
             user = user.uid;
@@ -100,9 +96,9 @@
 
                 } else {
                     db.ref(`/${user}/events`).once('value').then(function (d) {
-                        d.forEach(f =>{
+                        d.forEach(f => {
                             let current = f.val();
-                            if(current === currentEvent.data('id')){
+                            if (current === currentEvent.data('id')) {
                                 currentEvent.data('action', "add").html(`&#43; Add to My Events`).removeClass('btn-danger').addClass('btn-success');
                                 db.ref(`/${user}/events/${f.key}`).remove();
 
@@ -111,7 +107,7 @@
                     })
                 }
             })
-            selectedEventRender = function(eventInfo) {
+            selectedEventRender = function (eventInfo) {
                 let eventDiv = $('.selected-event');
                 eventDiv.empty();
                 eventDiv.append($('<div class="row loader">').append($('<img class="img-responsive">').attr('src', './assets/images/Loading_icon.gif')));
@@ -125,31 +121,31 @@
                 let address = $('<p>').text(eventInfo.address);
                 let description = $('<p>').text(eventInfo.description);
                 let addToEvents = "";
-                let getTickets = $('<button class="btn btn-primary add-to-event">').text("Get Tickets").data({ id: eventInfo.id, event: eventInfo.directLink});
+                let getTickets = $('<button class="btn btn-primary add-to-event">').text("Get Tickets").data({ id: eventInfo.id, event: eventInfo.directLink });
 
                 console.log(firebase.auth().currentUser);
-                    db.ref(`/${firebase.auth().currentUser.uid}/events`).once('value', function (d) {
-                        d.forEach(e => {
-                           let f = e.val();
-                            if (f === eventInfo.id) {
-                                addToEvents = $('<button class="btn btn-danger add-to-event">').html(`&times; Remove from Events`).data({ id: eventInfo.id, event: eventInfo.directLink, action: "delete" });
-                            }
-                        })
-                        if (!addToEvents) {
-                            addToEvents = $('<button class="btn btn-success add-to-event">').html(`&#43; Add to My Events`).data({ id: eventInfo.id, event: eventInfo.directLink, action: "add" });
+                db.ref(`/${firebase.auth().currentUser.uid}/events`).once('value', function (d) {
+                    d.forEach(e => {
+                        let f = e.val();
+                        if (f === eventInfo.id) {
+                            addToEvents = $('<button class="btn btn-danger add-to-event">').html(`&times; Remove from Events`).data({ id: eventInfo.id, event: eventInfo.directLink, action: "delete" });
                         }
-                        col_2.append(title, venue, address, description, getTickets,addToEvents);
-                        setTimeout(function () {
+                    })
+                    if (!addToEvents) {
+                        addToEvents = $('<button class="btn btn-success add-to-event">').html(`&#43; Add to My Events`).data({ id: eventInfo.id, event: eventInfo.directLink, action: "add" });
+                    }
+                    col_2.append(title, venue, address, description, getTickets, addToEvents);
+                    setTimeout(function () {
                         eventDiv.empty();
                         row.append(col_1, col_2);
                         eventDiv.append(row);
-                        }, 1000)
-                        
-                    })
-                    
-                }
-                
-            
+                    }, 1000)
+
+                })
+
+            }
+
+
         } else {
             $('.authentication').show();
             $('.logged-in-only').hide();
@@ -160,7 +156,7 @@
 
                 window.open($(this).data('event'));
             })
-            selectedEventRender = function(eventInfo) {
+            selectedEventRender = function (eventInfo) {
                 let eventDiv = $('.selected-event');
                 eventDiv.empty();
                 eventDiv.append($('<div class="row loader">').append($('<img class="img-responsive">').attr('src', './assets/images/Loading_icon.gif')));
@@ -173,14 +169,14 @@
                 let venue = $('<p>').text(eventInfo.venue);
                 let address = $('<p>').text(eventInfo.address);
                 let description = $('<p>').text(eventInfo.description);
-                let addToEvents = $('<button class="btn btn-primary add-to-event">').text("Get Tickets").data({ id: eventInfo.id, event: eventInfo.directLink});
+                let addToEvents = $('<button class="btn btn-primary add-to-event">').text("Get Tickets").data({ id: eventInfo.id, event: eventInfo.directLink });
                 col_2.append(title, venue, address, description, addToEvents);
                 setTimeout(function () {
-                eventDiv.empty();
-                row.append(col_1, col_2);
-                eventDiv.append(row);
+                    eventDiv.empty();
+                    row.append(col_1, col_2);
+                    eventDiv.append(row);
                 }, 1000)
-                }
+            }
         }
     })
     $(document).on('click', '.sign-out', function () {
@@ -220,95 +216,93 @@
                 if (!oData.events) {
                     $('#loading').addClass('display');
                     $('#location').val('').focus();
-                    $('#location').popover({content: 'Location not found. Please try again.', trigger: 'focus click', placement: 'bottom'});
+                    $('#location').popover({ content: 'Location not found. Please try again.', trigger: 'focus click', placement: 'bottom' });
                     $('#location').popover('show');
-                    
-                }else{
-                lat = parseFloat(oData.events.event[0].latitude);
-                long = parseFloat(oData.events.event[0].longitude)
-                console.log(oData);
 
-                getEventWeather(long, lat);
+                } else {
+                    lat = parseFloat(oData.events.event[0].latitude);
+                    long = parseFloat(oData.events.event[0].longitude)
+                    console.log(oData);
 
-                // var weatherObj = {
-                //     url: "https://api.openweathermap.org/data/2.5/weather",
-                //     method: "GET",
-                //     data: {
-                //         appid: weatherAPI,
-                //         lat: lat,
-                //         lon: long
-                //     },
-                // };
+                    getEventWeather(long, lat);
 
-                // Weather AJAX Call
-                // $.ajax(weatherObj).then(data => {
-                //     var tempConverted = parseInt((data.main.temp * (9 / 5) - 459.67));
-                //     var sunriseTime = moment.unix(data.sys.sunrise).format("HH:mm");
-                //     var sunsetTime = moment.unix(data.sys.sunset).format("HH:mm");
-                //     console.log("sunrise: " + sunriseTime);
-                //     console.log("sunset: " + sunsetTime);
-                //     console.log(now.diff(moment(data.sys.sunrise), "hours"));
-                //     $("#temp").append(tempConverted);
-                // });
+                    // var weatherObj = {
+                    //     url: "https://api.openweathermap.org/data/2.5/weather",
+                    //     method: "GET",
+                    //     data: {
+                    //         appid: weatherAPI,
+                    //         lat: lat,
+                    //         lon: long
+                    //     },
+                    // };
 
-                let eventData = {
-                    title: oData.events.event[0].title,
-                    venue_name: oData.events.event[0].venue_name,
-                    venue_address: oData.events.event[0].venue_address,
-                    city_name: oData.events.event[0].city_name,
+                    // Weather AJAX Call
+                    // $.ajax(weatherObj).then(data => {
+                    //     var tempConverted = parseInt((data.main.temp * (9 / 5) - 459.67));
+                    //     var sunriseTime = moment.unix(data.sys.sunrise).format("HH:mm");
+                    //     var sunsetTime = moment.unix(data.sys.sunset).format("HH:mm");
+                    //     console.log("sunrise: " + sunriseTime);
+                    //     console.log("sunset: " + sunsetTime);
+                    //     console.log(now.diff(moment(data.sys.sunrise), "hours"));
+                    //     $("#temp").append(tempConverted);
+                    // });
 
-                }
-
-                initMap(lat, long, eventData);
-                if ($('.carousel').flickity()) {
-                    $('.carousel').flickity('destroy');
-                }
-                $('.carousel').empty();
-                oData.events.event.forEach((event) => {
                     let eventData = {
-                        lat: event.latitude,
-                        long: event.longitude,
-                        title: event.title,
-                        address: event.venue_address,
-                        venue: event.venue_name,
-                        description: event.description,
-                        city: event.city_name,
-                        id: event.id,
-                        directLink: event.url,
-                        performers: event.performers,
-                        image: event.image ? event.image.medium.url : "./assets/images/out&about.jpg"
-
+                        title: oData.events.event[0].title,
+                        venue_name: oData.events.event[0].venue_name,
+                        venue_address: oData.events.event[0].venue_address,
+                        city_name: oData.events.event[0].city_name,
 
                     }
-                    let wrapper = $('<div class="carousel-cell">');
-                    let card = $('<div class="card">');
-                    let cardImg = $('<img class="img-responsive card-img-top">').attr('src', event.image ? event.image.medium.url : "./assets/images/out&about.jpg");
-                    let cardBody = $('<div class="card-body">');
-                    let title = $('<h6>').text(event.title);
-                    let venue_name = $('<p>').text(event.venue_name);
-                    let venue_address = $('<p>').text(event.venue_address).on('click', (e) => {
-                        initMap(parseFloat(eventData.lat), parseFloat(eventData.long), { title: event.title, venue_name: event.venue_name, venue_address: event.venue_address, city_name: event.city_name })
-                    });
-                    let city_name = $('<p>').text(event.city_name);
-                    let moreInfo = $('<button class="btn btn-primary">').text("More info").on('click', () => {
-                        selectedEventRender(eventData);
+
+                    initMap(lat, long, eventData);
+                    if ($('.carousel').flickity()) {
+                        $('.carousel').flickity('destroy');
+                    }
+                    $('.carousel').empty();
+                    oData.events.event.forEach((event) => {
+                        let eventData = {
+                            lat: event.latitude,
+                            long: event.longitude,
+                            title: event.title,
+                            address: event.venue_address,
+                            venue: event.venue_name,
+                            description: event.description,
+                            city: event.city_name,
+                            id: event.id,
+                            directLink: event.url,
+                            performers: event.performers,
+                            image: event.image ? event.image.medium.url : "./assets/images/out&about.jpg"
+
+
+                        }
+                        let wrapper = $('<div class="carousel-cell">');
+                        let card = $('<div class="card">');
+                        let cardImg = $('<img class="img-responsive card-img-top">').attr('src', event.image ? event.image.medium.url : "./assets/images/out&about.jpg");
+                        let cardBody = $('<div class="card-body">');
+                        let title = $('<h6>').text(event.title);
+                        let venue_name = $('<p>').text(event.venue_name);
+                        let venue_address = $('<p>').text(event.venue_address).on('click', (e) => {
+                            initMap(parseFloat(eventData.lat), parseFloat(eventData.long), { title: event.title, venue_name: event.venue_name, venue_address: event.venue_address, city_name: event.city_name })
+                        });
+                        let city_name = $('<p>').text(event.city_name);
+                        let moreInfo = $('<button class="btn btn-primary">').text("More info").on('click', () => {
+                            selectedEventRender(eventData);
+                        })
+                        cardBody.append(title, venue_name, venue_address, city_name, moreInfo);
+                        card.append(cardImg, cardBody);
+                        wrapper.append(card);
+                        $('.carousel').append(wrapper);
                     })
-                    cardBody.append(title, venue_name, venue_address, city_name, moreInfo);
-                    card.append(cardImg, cardBody);
-                    wrapper.append(card);
-                    $('.carousel').append(wrapper);
-                })
-                $('.carousel').flickity({ autoPlay: true, adaptiveHeight: true, setGallerySize: false });
-                $('#loading').addClass('display');
-            }
-            }).catch(err =>{
+                    $('.carousel').flickity({ autoPlay: true, adaptiveHeight: true, setGallerySize: false });
+                    $('#loading').addClass('display');
+                }
+            }).catch(err => {
                 $('#loading').addClass('display');
                 $('#location').val('').focus();
-                $('#location').popover({content: 'Location not found. Please try again.'});
+                $('#location').popover({ content: 'Location not found. Please try again.' });
                 $('#location').popover('show');
             })
-<<<<<<< HEAD
-=======
 
         // Weather AJAX Call
         $.ajax(weatherObj).then(data => {
@@ -320,13 +314,12 @@
             console.log(now.diff(moment(data.sys.sunrise), "hours"));
             $("#temp").append(tempConverted);
         });
-        
 
->>>>>>> 1d3eca5db99a9fba63f91372c7676e010420bc48
+
     }
     const categ = document.querySelector('#category');
     $('#location').on('click', function (e) {
-        
+
         let self = $('#category');
 
 
@@ -379,12 +372,7 @@
             `);
         });
     }
-<<<<<<< HEAD
 
-
-=======
-    
->>>>>>> 1d3eca5db99a9fba63f91372c7676e010420bc48
 
 
 
